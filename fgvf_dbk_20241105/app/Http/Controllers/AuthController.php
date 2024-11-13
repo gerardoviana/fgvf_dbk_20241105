b4bdec3239979e3f4ae6fc1f0680a0ebc3a844c7
Felipe Gerardo Viana Figueroa <gerardo_viana@hotmail.com>
2024-11-12 20:12:42 -0600

    Creacion y prueba de api con 3 endpoints

--- fgvf_dbk_20241105/app/Http/Controllers/AuthController.php
+++ fgvf_dbk_20241105/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -37,5 +38,51 @@ class AuthController extends Controller
             "msg"=>"Usuario creado satisfactoriamente",
             "count"=>1
         ]);
+    }
+
+
+
+    public function login(Request $request) {
+        if(!Auth::attempt($request->only( "email","password"))){
+            return response()->json( [
+                "success"=> false,
+                "errors"=>[
+                    "code"=>401,
+                    "msg"=> "No se reconocen las credenciales"
+                ],
+                "data"=>"",
+                "count"=> 0
+            ], 401);
+
+        }
+        $user = User::where("email", $request->email)->firstOrFail();
+        $token = $user->createToken("auth_token")->plainTextToken;
+
+        return response()->json( [
+            "success"=> true,
+            "errors"=>[
+                "code"=>200,
+                "msg"=> ""
+            ],
+            "data"=>[
+                "access_token"=>$token,
+                "token_type"=> "Bearer"
+            ],
+            "count"=> 1
+        ], 200);
+    
+    
+    }
+
+    public function me(Request $request) {
+        return response()->json( [
+            "success"=> true,
+            "errors"=>[
+                "code"=>200,
+                "msg"=> ""
+            ],
+            "data"=>$request->user(),
+            "count"=> 1
+        ], 200);
     }
 }
